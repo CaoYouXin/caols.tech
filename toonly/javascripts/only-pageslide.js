361688469fe022832b7c950b524b5a60891a77f3
CaoYouXin <c_youxin@163.com>
2016-03-29 18:57:54 +0800

    toonly-slide

--- toonly/javascripts/only-pageslide.js
+++ toonly/javascripts/only-pageslide.js
@@ -3,19 +3,8 @@
  */
 define(['jquery', 'handlebars'], function ($, handlebars) {
 
-    var urlStack = [], curX = 0;
+    var urlStack = [];
     var pageslideTplFn = handlebars.compile('{{#each this}}<li class="pageslide"><iframe src="{{this}}" frameborder="0"></iframe></li>{{/each}}');
-    var controlsTplFn = handlebars.compile('<a href="#" id="left" style="position: fixed;z-index: 999999999;left: 0;bottom: 0;padding: 10px;border-radius: 10px;">左</a>' +
-        '<a href="#" id="right" style="position: fixed;z-index: 999999999;right: 0;bottom: 0;padding: 10px;border-radius: 10px;">右</a>');
-
-    $('body').append(controlsTplFn());
-    $('ul.pageslides').css('transform', 'translateX(' + (curX += $(window).width()) + 'px)');
-
-    function genEqualFn(url) {
-        return function (item) {
-            return url === item;
-        }
-    }
 
     return {
         init: function () {
@@ -26,34 +15,22 @@ define(['jquery', 'handlebars'], function ($, handlebars) {
             var windowWidth = $(window).width();
             var $lis = $ul.children('li.pageslide');
             var ulWidth = windowWidth * $lis.length;
-            $lis.width(windowWidth);
-            $ul.width(ulWidth);
-
-            $('#left').click(function () {
-                if (curX <= (0 - ulWidth + windowWidth)) {
-                    return false;
-                }
-                $ul.css('transform', 'translateX(' + (curX -= windowWidth) + 'px)');
-            });
-
-            $('#right').click(function () {
-                if (curX >= 0) {
-                    return false;
-                }
-                $ul.css('transform', 'translateX(' + (curX += windowWidth) + 'px)');
-            });
+            $lis.css('width', windowWidth + 'px');
+            $ul.css('width', ulWidth + 'px');
         },
         go: function (url) {
-            var hasUrl = urlStack.some(genEqualFn(url));
-            if (hasUrl) {
-                var indexOf = urlStack.indexOf(url);
-
-                $('ul.pageslides').css('transform', 'translateX(' + (0 - (indexOf * $(window).width())) + 'px)');
-            } else {
+            var indexOf = urlStack.indexOf(url);
+            if (indexOf === -1) {
+                indexOf = urlStack.length;
                 urlStack.push(url);
-                this.init();
-                $('#left').click();
             }
+
+            this.init();
+            var $ul = $('ul.pageslides');
+            var children = $ul.children('li.pageslide');
+            children.removeClass('active');
+            children.eq(indexOf).addClass('active');
+            $ul.css('transform', 'translateX(' + (0 - (indexOf * $(window).width())) + 'px)');
         }
     };
 });
