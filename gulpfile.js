a290cd952051c8904d85c1fa1d8094839f5a7aaa
曹力升 <c_youxin@163.com>
2016-09-19 21:31:07 +0800

    build system

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ const imageSrc = './images/';
 const imageDst = '../images/';
 
 function jsDefault(src, dst) {
-    gulp.src(src)
+    return gulp.src(src)
         .pipe(uglifyJs())
         .pipe(rename(function (path) {
             path.basename += ".min";
@@ -24,17 +24,21 @@ function jsDefault(src, dst) {
 }
 
 gulp.task('default', function() {
-    gulp.start('b-ps', 'b-3d');
+    gulp.start('b-html', 'b-js', 'b-css', 'posts', 'cdn', 'b-others');
 });
 
-gulp.task('b-ps', function() {
+gulp.task('b-ps-js', function() {
     var jsSrc = __devSrc + 'pageslider/*.js';
+    var psDst = _devSrc + 'js/pageslider/';
+
+    return jsDefault(jsSrc, psDst);
+});
+
+gulp.task('b-ps-css', function() {
     var cssSrc = __devSrc + 'pageslider/*.css';
-    var psDst = dst + 'pageslider/';
+    var psDst = _devSrc + 'css/pageslider/';
 
-    jsDefault(jsSrc, psDst);
-
-    gulp.src(cssSrc)
+    return gulp.src(cssSrc)
         .pipe(minifyCss())
         .pipe(rename(function (path) {
             path.basename += ".min";
@@ -44,23 +48,39 @@ gulp.task('b-ps', function() {
 
 gulp.task('b-3d', function () {
     var jsSrc = __devSrc + '3d/*.js';
-    var _3dDst = dst + '3d/';
+    var _3dDst = _devSrc + 'js/3d/';
 
-    jsDefault(jsSrc, _3dDst);
+    return jsDefault(jsSrc, _3dDst);
+});
+
+gulp.task('b-pu', function () {
+    var promiseSrc = __devSrc + 'promise/*.js';
+    var promiseDst = _devSrc + 'js/promise/';
+
+    return jsDefault(promiseSrc, promiseDst);
 });
 
 gulp.task('b-html', function () {
-    var htmlSrc = _devSrc + '*/*.html';
+    var htmlSrc = _devSrc + '*.html';
 
-    gulp.src([htmlSrc, _devSrc + '*.html'])
+    gulp.src(htmlSrc)
         .pipe(gulp.dest(dst));
 });
 
-gulp.task('b-js', function () {
-    var promiseSrc = __devSrc + 'promise/*.js';
-    var promiseDst = dst + '/promise/';
+gulp.task('b-js', ['b-3d', 'b-pu', 'b-ps-js'], function () {
+    var jsSrc = _devSrc + 'js/';
+    var jsDst = dst + '/js/';
 
-    jsDefault(promiseSrc, promiseDst);
+    gulp.src([jsSrc + '*.js', jsSrc + '*/*.js'], {base: jsSrc})
+        .pipe(gulp.dest(jsDst));
+});
+
+gulp.task('b-css', ['b-ps-css'], function () {
+    var jsSrc = _devSrc + 'css/';
+    var jsDst = dst + '/css/';
+
+    gulp.src([jsSrc + '*.css', jsSrc + '*/*.css'], {base: jsSrc})
+        .pipe(gulp.dest(jsDst));
 });
 
 gulp.task('posts', function () {
@@ -76,4 +96,11 @@ gulp.task('cdn', function () {
     gulp.src(imageSrc).pipe(gulp.dest(imageDst));
 
     exec('cd '+imageDst+';git checkout master;git add *.png;git add *.jpg;git commit -m "add images";git push origin master;');
+});
+
+gulp.task('b-others', function () {
+    var htmlSrc = _devSrc + 'x-handlebars-templates/*.html';
+
+    gulp.src(htmlSrc, {base: _devSrc})
+        .pipe(gulp.dest(dst));
 });
