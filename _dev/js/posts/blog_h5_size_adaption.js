fd83c963199349cf5c857a5fa3bf86ad52e5eaa4
曹力升 <c_youxin@163.com>
2016-09-23 22:12:42 +0800

    多说+jsloader

--- _dev/js/posts/blog_h5_size_adaption.js
+++ _dev/js/posts/blog_h5_size_adaption.js
@@ -1,7 +1,8 @@
 /**
  * Created by cls on 16/9/23.
  */
-;(function () {
+;(function (P, rootHref) {
 
+    P.script(document, rootHref + 'build/js/duoshuo.js');
 
-})();
+})(window.top.ES6Promise.Promise, window.top.Router.rootHref);
