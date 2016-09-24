7272de68d8efdfb3290eb2b13eb4db3330cc41c3
曹力升 <c_youxin@163.com>
2016-09-24 11:01:15 +0800

    image replacement src,href,url

--- gulp-images-replacement.js
+++ gulp-images-replacement.js
@@ -6,6 +6,7 @@ module.exports = function () {
 
     var urlRegExp = /url\(('|").*images\/(.*)('|")\)/g;
     var hrefRegExp = /href=('|").*images\/(.*)('|")/g;
+    var srcRegExp = /src=('|").*images\/(.*)('|")/g;
 
     return through(function (file, encoding, cb) {
         var contents = file.contents.toString();
@@ -13,6 +14,8 @@ module.exports = function () {
             return 'url('+$1+'http://image.caols.tech/'+$2+$3+')';
         }).replace(hrefRegExp, function ($0, $1, $2, $3) {
             return 'href='+$1+'http://image.caols.tech/'+$2+$3;
+        }).replace(srcRegExp, function ($0, $1, $2, $3) {
+            return 'src='+$1+'http://image.caols.tech/'+$2+$3;
         }));
 
         this.push(file);
