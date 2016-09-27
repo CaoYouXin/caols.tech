d0961c69b81e6567f9db67f4282f8b17808d2687
曹力升 <c_youxin@163.com>
2016-09-27 20:02:53 +0800

    other interview

--- _dev/js/posts/blog_interview_js.js
+++ _dev/js/posts/blog_interview_js.js
@@ -6,34 +6,43 @@
     P.script(document, rootHref + 'build/js/post.js');
 
     var apiElem = document.querySelector('table#api > tbody');
-    var tr = document.createElement('tr');
-    var td = document.createElement('td');
 
     function f(k) {
         return typeof k === 'string' && !k.match(/__/);
     }
 
-    for (var arrayKeys = Reflect.ownKeys(Array.prototype).filter(f),
-             objectKeys = Reflect.ownKeys(Object.prototype).filter(f),
-             // reflectKeys = Reflect.ownKeys(Reflect.prototype).filter(f),
-             count = Math.max(arrayKeys.length, objectKeys.length),
-             i = 0, aK = arrayKeys[i], oK = objectKeys[i]; i < count;
-         i++, aK = arrayKeys[i], oK = objectKeys[i]) {
+    var tr, td;
+    try {
+        for (var arrayKeys = Reflect.ownKeys(Array.prototype).filter(f),
+                 objectKeys = Reflect.ownKeys(Object.prototype).filter(f),
+                 // reflectKeys = Reflect.ownKeys(Reflect.prototype).filter(f),
+                 count = Math.max(arrayKeys.length, objectKeys.length),
+                 i = 0, aK = arrayKeys[i], oK = objectKeys[i]; i < count;
+             i++, aK = arrayKeys[i], oK = objectKeys[i]) {
 
-        var tr = document.createElement('tr');
+            tr = document.createElement('tr');
+            apiElem.appendChild(tr);
+
+            td = document.createElement('td');
+            td.innerHTML = !!aK ? aK.toString() : '';
+            tr.appendChild(td);
+
+            td = document.createElement('td');
+            td.innerHTML = !!oK ? oK.toString() : '';
+            tr.appendChild(td);
+
+            // td = document.createElement('td');
+            // td.innerHTML = !!rK ? rK.toString() : '';
+            // tr.appendChild(td);
+        }
+    } catch (e) {
+        tr = document.createElement('tr');
         apiElem.appendChild(tr);
 
-        var td = document.createElement('td');
-        td.innerHTML = !!aK ? aK.toString() : '';
-        tr.appendChild(td);
-
         td = document.createElement('td');
-        td.innerHTML = !!oK ? oK.toString() : '';
+        td.setAttribute('colspan', '2');
+        td.innerHTML = '你的浏览器不支持Reflect API!';
         tr.appendChild(td);
-
-        // td = document.createElement('td');
-        // td.innerHTML = !!rK ? rK.toString() : '';
-        // tr.appendChild(td);
     }
 
 })(window.top.ES6Promise.Promise, window.top.Router.rootHref);
