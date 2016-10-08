ec45777367ef3b401564be00a88b39f10c4c798b
曹力升 <c_youxin@163.com>
2016-10-08 10:01:45 +0800

    能力图谱，第三波。

--- _dev/js/posts/blog_abilities.js
+++ _dev/js/posts/blog_abilities.js
@@ -10,7 +10,7 @@
 
         for (var keys = Object.keys(obj), size = keys.length, i = 0; i < size; i++) {
             ret.push({
-                axis: keys[i],
+                axis: keys[i].replace(/或/, '或\n'),
                 value: obj[keys[i]]
             });
         }
@@ -18,15 +18,83 @@
         return ret;
     }
 
-    P.ajax(rootHref + 'build/json/survey-14220.csv.json').then(function (rawData) {
-        var parsedData = JSON.parse(rawData);
-
-        var margin = {top: 100, right: 100, bottom: 100, left: 100},
+    function draw(el, title, data, maxSpi) {
+        var maxSpi = maxSpi || 5,
+            margin = {top: 100, right: 100, bottom: 100, left: 100},
             width = Math.min(700, document.querySelector('.post-content').offsetWidth - 10) - margin.left - margin.right,
             height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
 
-        //Data
-        var data = [parseData(parsedData.ability), parseData({
+        var color = d3.scale.ordinal()
+            .range(["#EDC951", "#00A0B0"]);
+        var LegendOptions = ['他人评价', '自我评价'];
+
+        var multiFactor = Math.floor(maxSpi / 5);
+        var radarChartOptions = {
+            w: width,
+            h: height + 10,
+            margin: margin,
+            maxValue: 5 * (maxSpi % 5 === 0 ? multiFactor : multiFactor + 1),
+            levels: 5,
+            roundStrokes: true,
+            color: color
+        };
+        //Call function to draw the Radar chart
+        RadarChart(el, data, radarChartOptions);
+
+        var svg = d3.select(el + ' > svg');
+
+        //Create the title for the legend
+        var text = svg.append("text")
+            .attr("class", "title")
+            .attr('transform', 'translate(90,20)')
+            .attr("x", width - 70)
+            .attr("y", 10)
+            .attr("font-size", "12px")
+            .attr("fill", "#404040")
+            .text(title);
+
+        //Initiate Legend
+        var legend = svg.append("g")
+            .attr("class", "legend")
+            .attr("height", 100)
+            .attr("width", 200)
+            .attr('transform', 'translate(90,40)');
+
+        //Create colour squares
+        legend.selectAll('rect')
+            .data(LegendOptions)
+            .enter()
+            .append("rect")
+            .attr("x", width - 65)
+            .attr("y", function (d, i) {
+                return i * 20;
+            })
+            .attr("width", 10)
+            .attr("height", 10)
+            .style("fill", function (d, i) {
+                return color(i);
+            });
+
+        //Create text next to squares
+        legend.selectAll('text')
+            .data(LegendOptions)
+            .enter()
+            .append("text")
+            .attr("x", width - 52)
+            .attr("y", function (d, i) {
+                return i * 20 + 9;
+            })
+            .attr("font-size", "11px")
+            .attr("fill", "#737373")
+            .text(function (d) {
+                return d;
+            });
+    }
+
+    P.ajax(rootHref + 'build/json/survey-14220.csv.json').then(function (rawData) {
+        var parsedData = JSON.parse(rawData);
+
+        draw('#chart1', '个人能力图谱', [parseData(parsedData.ability), parseData({
             "想象力": 4,
             "记忆力": 5,
             "观察能力": 4,
@@ -38,52 +106,24 @@
             "学习能力": 5,
             "号召能力": 2,
             "适应能力": 3
-        })];
+        })]);
 
-        var color = d3.scale.ordinal()
-            .range(["#EDC951", "#00A0B0"]);
-        var LegendOptions = ['他人评价', '自我评价'];
+        draw('#chart2', '行为能力图谱', [parseData(parsedData.lawAbility), parseData({
+            "面对一般事物或行为的判断力": 5,
+            "面对一般事物或行为的自我保护能力": 3,
+            "面对重大事物或行为的判断力": 3,
+            "面对重大事物或行为的自我保护能力": 4,
+            "面对较复杂事物或行为的判断力": 4,
+            "面对较复杂事物或行为的自我保护能力": 4
+        })]);
 
-        var radarChartOptions = {
-            w: width,
-            h: height,
-            margin: margin,
-            maxValue: 5,
-            levels: 5,
-            roundStrokes: true,
-            color: color
-        };
-        //Call function to draw the Radar chart
-        RadarChart("#chart", data, radarChartOptions);
-
-        //Initiate Legend
-        var legend = d3.select('#chart > svg').append("g")
-                .attr("class", "legend")
-                .attr("height", 100)
-                .attr("width", 200)
-                .attr('transform', 'translate(90,20)');
-
-        //Create colour squares
-        legend.selectAll('rect')
-            .data(LegendOptions)
-            .enter()
-            .append("rect")
-            .attr("x", width - 65)
-            .attr("y", function(d, i){ return i * 20;})
-            .attr("width", 10)
-            .attr("height", 10)
-            .style("fill", function(d, i){ return color(i);});
-
-        //Create text next to squares
-        legend.selectAll('text')
-            .data(LegendOptions)
-            .enter()
-            .append("text")
-            .attr("x", width - 52)
-            .attr("y", function(d, i){ return i * 20 + 9;})
-            .attr("font-size", "11px")
-            .attr("fill", "#737373")
-            .text(function(d) { return d; });
+        var spi = parseData(parsedData.spi), maxSpi = 0;
+        for (var i = 0, size = spi.length; i < size; i++ ) {
+            if (maxSpi < spi[i].value) {
+                maxSpi = spi[i].value;
+            }
+        }
+        draw('#chart3', '可能具有的超能力', [spi, spi], maxSpi);
     });
 
 })(window.top.ES6Promise.Promise, window.top.Router.rootHref, window.top.Router, window.top.PageSlider);
