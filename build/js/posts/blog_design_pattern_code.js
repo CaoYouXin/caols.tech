c7472ff19cd1a72dae7e0dde5a395b1c29449feb
曹力升 <c_youxin@163.com>
2016-11-26 14:54:54 +0800

    设计模式

---
+++ build/js/posts/blog_design_pattern_code.js
@@ -0,0 +1,15 @@
+/**
+ * Created by cls on 2016/11/26.
+ */
+;(function (P, rootHref, R, PS) {
+
+    P.all([
+        P.script(document, rootHref + '3rdLib/prism/prism.min.js'),
+        P.script(document, rootHref + 'build/js/post.js')
+    ]).then(function (values) {
+        for (var i = 0, codes = document.getElementsByTagName('code'); i < codes.length; i++) {
+            codes[i].innerHTML = window.Prism.highlight(codes[i].innerHTML, window.Prism.languages.javascript);
+        }
+    });
+
+})(window.top.ES6Promise.Promise, window.top.Router.rootHref, window.top.Router, window.top.PageSlider);
