db6ea891ce3880b2f52282918c8950404960b11b
曹力升 <c_youxin@163.com>
2016-11-26 08:56:26 +0800

    fix design pattern blog

---
+++ _dev/js/posts/blog_design_pattern.js
@@ -0,0 +1,8 @@
+/**
+ * Created by cls on 2016/11/26.
+ */
+;(function (P, rootHref, R, PS) {
+
+    P.script(document, rootHref + 'build/js/post.js');
+
+})(window.top.ES6Promise.Promise, window.top.Router.rootHref, window.top.Router, window.top.PageSlider);
