b68bc0c44bb7373410ebb7b145220cb2688e0056
曹力升 <c_youxin@163.com>
2016-11-28 18:36:45 +0800

    添加一些游戏

--- gulp-images-replacement.js
+++ gulp-images-replacement.js
@@ -7,6 +7,7 @@ module.exports = function () {
     var urlRegExp = /url\(('|").*images\/(.*)('|")\)/g;
     var hrefRegExp = /href=('|").*images\/(.*)('|")/g;
     var srcRegExp = /src=('|").*images\/(.*)('|")/g;
+    var dataUrlRegExp = /data-url=('|").*images\/(.*)('|")/g;
 
     return through(function (file, encoding, cb) {
         var contents = file.contents.toString();
@@ -16,6 +17,8 @@ module.exports = function () {
             return 'href='+$1+'http://image.caols.tech/'+$2+$3;
         }).replace(srcRegExp, function ($0, $1, $2, $3) {
             return 'src='+$1+'http://image.caols.tech/'+$2+$3;
+        }).replace(dataUrlRegExp, function ($0, $1, $2, $3) {
+            return 'data-url='+$1+'http://image.caols.tech/'+$2+$3;
         }));
 
         this.push(file);
