7cab95b87fc29caff38d07b4aa70f13514fd0122
曹力升 <c_youxin@163.com>
2017-02-08 23:33:18 +0800

    append videobox to _3d

--- __dev/3d/3d_.js
+++ __dev/3d/3d_.js
@@ -28,6 +28,7 @@
     // }
 
     W._3d = function (el) {
+
         var canvas = this.Canvas = document.querySelector(el);
         this.Camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight, 1, 2000);
         this.Scene = new THREE.Scene();
@@ -42,6 +43,7 @@
                 window.setTimeout(callback, 1000 / 60);
             };
         this.Clock = new THREE.Clock();
+
     };
 
     W._3d.prototype.Action = function (statsFlag) {
@@ -83,62 +85,177 @@
         this.RequestFrame.call(W, this.Loop.bind(this));
     };
 
-    // {
-    // W: W,
-    // Text: function (texts) {
-    //     var s = 0, geo = null;
-    //
-    //     setFontSize(fontSize);
-    //     s = Math.min(fontSize,
-    //         (paintCanvas.width / paintCtx.measureText(texts[0]).width) * 0.8 * fontSize,
-    //         (paintCanvas.height / fontSize) * (isNumber(texts[0]) ? 1 : 0.45) * fontSize);
-    //     setFontSize(s);
-    //
-    //     paintCtx.clearRect(0, 0, paintCanvas.width, paintCanvas.height);
-    //     for (var i = 0; i < texts.length; i++) {
-    //         paintCtx.fillText(texts[i], 0, s * (i + 1));
-    //     }
-    //
-    //     var pixels = paintCtx.getImageData(0, 0, paintCanvas.width, paintCanvas.height).data,
-    //         dots = [],
-    //         x = 0,
-    //         y = 0,
-    //         fx = paintCanvas.width,
-    //         fy = paintCanvas.height,
-    //         w = 0,
-    //         h = 0;
-    //
-    //     for (var p = 0; p < pixels.length; p += (4 * gap)) {
-    //         if (pixels[p + 3] > 0) {
-    //             if (!geo) {
-    //                 geo = new T.Geometry();
-    //             }
-    //
-    //             for (var z = 0; z < 10; z++) {
-    //                 var particle = new THREE.Vector3(x / factor, -y / factor, z / factor);
-    //                 geo.vertices.push(particle);
-    //             }
-    //         }
-    //
-    //         x += gap;
-    //
-    //         if (x >= paintCanvas.width) {
-    //             x = 0;
-    //             y += gap;
-    //             p += gap * 4 * paintCanvas.width;
-    //         }
-    //     }
-    //
-    //     var material = new THREE.PointsMaterial(
-    //         {
-    //             color: '#ffffff',
-    //             size: gap / factor,
-    //             blending: THREE.AdditiveBlending,
-    //             transparent: true
-    //         });
-    //
-    //     return new THREE.Points(geo, material);
-    // }
-    // };
+    W._3d.prototype.ProgressBar = function (pbEl) {
+
+        var pbElement = document.querySelector(pbEl), upLen, up = 0, _totalLen, _cb = function () {
+        }, _params = [], did = false;
+        var self = this;
+
+        return {
+            init: function (totalLen) {
+                upLen = 100 / totalLen;
+                _totalLen = totalLen;
+            },
+            upTogo: function (high) {
+                if (!!high) {
+                    up = high / _totalLen * 100;
+                } else {
+                    up += upLen;
+                }
+                pbElement.innerHTML = up.toFixed(2) + '% completed';
+
+                if (!did && 99.9 <= up) {
+                    did = !did;
+
+                    pbElement.style.display = 'none';
+
+                    _cb.apply(null, _params);
+                }
+            },
+            cb: function (cb, params) {
+                _cb = cb;
+                _params = params || [];
+            }
+        };
+
+    };
+
+    W._3d.prototype.Group = function (position) {
+
+        var _position = position, root = new THREE.Object3D();
+
+        root.position.x = _position.x;
+        root.position.y = _position.y;
+        root.position.z = _position.z;
+
+        this.Scene.add(root);
+
+        function set(mesh, pOffset, rotation) {
+            mesh.position.x = pOffset.x;
+            mesh.position.y = pOffset.y;
+            mesh.position.z = pOffset.z;
+
+            mesh.updateMatrix();
+
+            if (Object.prototype.toString.call(rotation) === '[object Array]') {
+
+                mesh.rotation.set.apply(null, rotation);
+
+                mesh.updateMatrix();
+            }
+        }
+
+        return {
+            add: function (mesh, pOffset, rotation) {
+
+                set(mesh, pOffset, rotation);
+
+                root.add(mesh);
+
+            },
+            update: function (new_position, rotation) {
+
+                set(root, new_position, rotation);
+
+            }
+        };
+
+    };
+
+    W._3d.prototype.VideoBox = function () {
+
+        var self = this;
+        self.globalCursor = 0;
+
+        function findFrameLinear() {
+            if (!self.aniSeq.length) {
+                return null;
+            }
+
+            while (self.aniSeq[self.aniCursor].accumulatedDur < self.globalCursor) self.aniCursor++;
+
+            var curKeyFrame = self.aniSeq[self.aniCursor];
+
+            var duration = self.globalCursor - (self.aniCursor <= 0 ? 0 : self.aniSeq[self.aniCursor - 1].accumulatedDur);
+
+        }
+
+        return {
+            init: function (ratio, keyProps) {
+                self.ratio = ratio;
+                self.keyProps = keyProps;
+
+                self.aniSeq = [];
+                self.aniCursor = 0;
+
+                self.lastKeyFrame = null;
+
+                return {
+                    add: function (data) {//contains time prop, type: int
+                        data.duration = 1000;
+                        data.accumulatedDur = 0;
+
+                        if (!self.lastKeyFrame) {
+                            self.lastKeyFrame = data;
+                            return this;
+                        } else {
+                            data.accumulatedDur = self.lastKeyFrame.accumulatedDur;
+                        }
+
+                        var duration = data.time - self.lastKeyFrame.time;
+
+                        self.keyProps.forEach(function (p) {
+                            self.lastKeyFrame['_v_' + p] = (data[p] -  self.lastKeyFrame[p]) / duration;
+                        });
+
+                        this.end(duration);
+
+                        return this;
+                    },
+                    end: function (duration) {
+
+                        if (duration !== undefined && duration > 0) {
+                            self.lastKeyFrame.duration = duration;
+                            self.lastKeyFrame.accumulatedDur += duration;
+                        } else {
+                            self.lastKeyFrame.accumulatedDur += self.lastKeyFrame.duration;
+                        }
+
+                        self.aniSeq.push(self.lastKeyFrame);
+                        self.needsUpdate = true;
+
+                        return this;
+                    },
+                    renderFn: function (fn) {
+                        self.renderFn = fn;
+
+                        return this;
+                    }
+                };
+            },
+            render: function (delta) {
+                self.globalCursor += delta;
+
+                var frame = findFrame();
+
+                self.renderFn(frame);
+            },
+            len: function () {
+                if (self.needsUpdate) {
+                    self.len = self.aniSeq[self.aniSeq.length - 1].accumulatedDur;
+                    self.needsUpdate = false;
+                }
+                return self.len;
+            },
+            goTo: function (time) {
+                self.globalCursor = time;
+
+                var frame = findFrame();
+
+                self.renderFn(frame);
+            }
+        };
+
+    };
 
 })(THREE, window);
