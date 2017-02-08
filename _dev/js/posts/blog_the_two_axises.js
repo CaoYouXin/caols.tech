d2ac28d0a47df6f533be5367f4561f5a9135622f
曹力升 <c_youxin@163.com>
2017-02-08 23:32:34 +0800

    create new blog named two axises

---
+++ _dev/js/posts/blog_the_two_axises.js
@@ -0,0 +1,13 @@
+;(function () {
+    'use strict';
+
+    //yac(sys.load_module)();
+    //yac(sys.load_default_module)();
+
+    yac(sys.init)(function (t) {
+        // 定义变量
+
+        // 功能代码
+
+    });
+}());
