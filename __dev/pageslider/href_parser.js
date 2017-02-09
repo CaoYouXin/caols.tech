3449b2c7260c0e213936775474fc1aa07492f6ca
曹力升 <c_youxin@163.com>
2017-02-09 13:32:35 +0800

    fix a small bug

--- __dev/pageslider/href_parser.js
+++ __dev/pageslider/href_parser.js
@@ -3,7 +3,7 @@
   window.top.parseHref = window.top.parseHref || function() {
     var domain = 'caols.tech';
     var isProductEnv = domain === document.domain;
-    var regExp = new RegExp('^http.*?\/\/.*?(\/.*?'+domain+'\/)(.*)$');
+    var regExp = new RegExp('^http.*?\/\/.*?(\/?.*?'+domain+'\/)(.*)$');
     var matched = window.location.href.toString().match(regExp);
     var rootHref = isProductEnv ? '/' : matched[1];
 
