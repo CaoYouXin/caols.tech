12c0cfecc108eabb2e85e68c1be9d20ff1dab3d9
曹力升 <c_youxin@163.com>
2017-04-21 16:23:51 +0800

    modify a.js

--- a.js
+++ a.js
@@ -1,1 +1,5 @@
 console.log('It is a honor to display a string from "caols.tech"!!!');
+
+document.getElementById("testBtn").addEventListener("click", function(){
+    document.getElementById("test").classList.toggle("hidden");
+});
