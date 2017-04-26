21d665c6ff311cfb49edd888257e1cae1e4d004b
曹力升 <c_youxin@163.com>
2017-04-26 19:52:12 +0800

    blog data 1

--- b.sh
+++ b.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+versionCfgFile="version.cfg"
+
+if [ -e ${versionCfgFile} ]; then
+	version=$(cat ${versionCfgFile})
+	echo ${version}
+else
+	version=0
+	echo ${versionCfgFile} file not exits
+fi
+
+version=$(expr ${version} + 1)
+echo ${version} > ${versionCfgFile}
+
+comment="blog data $version"
+
 gulp \
     && cp -r ./dist/ ./docs/ \
-    && git commit -am "自动添加"
+    && git commit -am "$comment"
+
+echo ${comment}
