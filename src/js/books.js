450786ef2718637d1eeae183ddc4891206ca6e63
曹力升 <c_youxin@163.com>
2017-04-29 20:55:43 +0800

    blog data v2.0.44

--- src/js/books.js
+++ src/js/books.js
@@ -45,44 +45,44 @@
         document.querySelector('.book-box:nth-child(' + (index + 2) + ') > .book-content').classList.toggle('active');
     };
 
-    P.all([
-        P.ajax(P.misc('http://localhost:8080/caols.tech/src/misc/json/books.json')),
-        P.ajax(P.misc('http://localhost:8080/caols.tech/src/misc/x-handlebars-templates/favorite_book_list.html'))
-    ]).then(function (values) {
+    // P.all([
+    //     P.ajax(P.misc('http://localhost:8080/caols.tech/src/misc/json/books.json')),
+    //     P.ajax(P.misc('http://localhost:8080/caols.tech/src/misc/x-handlebars-templates/favorite_book_list.html'))
+    // ]).then(function (values) {
         var codeElem = document.getElementById('code');
-        P.append(codeElem, highlight.highlightAuto(values[0]).value);
+        P.append(codeElem, highlight.highlightAuto("{}").value);
 
-        var tableBody = document.querySelector('#table > tbody');
-        var data = JSON.parse(values[0]);
-        var keys = ['name', 'author', 'category', 'publish_time', 'publish_org', 'introduction', 'state', 'cover_url'];
-        data.forEach(function (d) {
-            var tr = document.createElement('tr');
-
-            keys.forEach(function (k) {
-                var td = document.createElement('td');
-
-                if ('cover_url' === k) {
-                    td.innerHTML = '<div style="width: 100px;height: 150px;margin: 10px;background: rgba(0,0,0,.5) url(' + d[k] + ') no-repeat top;background-size: contain;"></div>';
-                } else {
-                    td.innerHTML = d[k];
-                }
-
-                tr.appendChild(td);
-            });
-
-            tableBody.appendChild(tr);
-
-            var number = ~~(Math.random() * (1 << 24));
-            var s = number.toString(16);
-            d.bgColor = '#' + '000000'.substring(s.length) + s;
-
-            d.color = number > (1 << 23) ? '#000000' : '#ffffff';
-        });
-
-        var favorite = document.getElementById('favorite');
-        P.append(favorite, H.compile(values[1])(data));
+        // var tableBody = document.querySelector('#table > tbody');
+        // var data = JSON.parse(values[0]);
+        // var keys = ['name', 'author', 'category', 'publish_time', 'publish_org', 'introduction', 'state', 'cover_url'];
+        // data.forEach(function (d) {
+        //     var tr = document.createElement('tr');
+        //
+        //     keys.forEach(function (k) {
+        //         var td = document.createElement('td');
+        //
+        //         if ('cover_url' === k) {
+        //             td.innerHTML = '<div style="width: 100px;height: 150px;margin: 10px;background: rgba(0,0,0,.5) url(' + d[k] + ') no-repeat top;background-size: contain;"></div>';
+        //         } else {
+        //             td.innerHTML = d[k];
+        //         }
+        //
+        //         tr.appendChild(td);
+        //     });
+        //
+        //     tableBody.appendChild(tr);
+        //
+        //     var number = ~~(Math.random() * (1 << 24));
+        //     var s = number.toString(16);
+        //     d.bgColor = '#' + '000000'.substring(s.length) + s;
+        //
+        //     d.color = number > (1 << 23) ? '#000000' : '#ffffff';
+        // });
+        //
+        // var favorite = document.getElementById('favorite');
+        // P.append(favorite, H.compile(values[1])(data));
 
         g.handlers.btnClicked(1);
-    });
+    // });
 
 })(window, window.P, window['Handlebars'], window['hljs']);
