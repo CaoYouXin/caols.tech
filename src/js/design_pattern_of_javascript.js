1a3abdb63bd3f0733924902cc71d64c623b727f2
曹力升 <c_youxin@163.com>
2017-04-30 08:44:56 +0800

    blog data v2.0.59

---
+++ src/js/design_pattern_of_javascript.js
@@ -0,0 +1,8 @@
+/**
+ * Created by cls on 2017/4/30.
+ */
+;(function (g, HL) {
+
+    HL.initHighlightingOnLoad();
+
+})(window, window['hljs']);
---
+++ src/js/knowledge_summery.js
@@ -0,0 +1,6 @@
+/**
+ * Created by cls on 2017/4/30.
+ */
+;(function (global) {
+
+})(window);
